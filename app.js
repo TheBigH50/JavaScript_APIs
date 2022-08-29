103c04c71c85413d0d74a8f093876214c9f08cd0
TheBigH50 <HFangmeier@outlook.com>
2022-08-29 15:10:52 -0500

    Exe1 final

--- app.js
+++ app.js
@@ -2,3 +2,7 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+let submitSearch = document.getElementById("submitSearch");
+let searchWord = document.getElementById("searchWord");
+let imageContainer = document.getElementById("imageContainer");
+let feedbackParagraph = document.getElementById("feedbackParagraph");
