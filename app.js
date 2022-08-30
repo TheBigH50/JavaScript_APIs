20ac1467fcb751b43526c6ccb7568dc3762bf8b0
TheBigH50 <HFangmeier@outlook.com>
2022-08-30 11:10:22 -0500

    Exe2

--- app.js
+++ app.js
@@ -6,3 +6,12 @@ let submitSearch = document.getElementById("submitSearch");
 let searchWord = document.getElementById("searchWord");
 let imageContainer = document.getElementById("imageContainer");
 let feedbackParagraph = document.getElementById("feedbackParagraph");
+let giphyURL = `https://api.giphy.com/v1/gifs/translate`;
+let APIKEY = `0z0AH2s7I80puVvSwg5jeyzJ8CUJnQav`;
+let S = searchWord.value;
+
+submitSearch.addEventListener("click", (event) => {
+    console.log("hello");
+fetch(`${giphyURL}?api_key=${APIKEY}&s=${S}`);
+});
+
