b6a411103f8a28208ecd142b6ad57e2668e488c4
TheBigH50 <HFangmeier@outlook.com>
2022-08-30 11:45:04 -0500

    Exe3,4

--- app.js
+++ app.js
@@ -4,14 +4,17 @@ console.log("Hello World!\n==========\n");
 console.log("EXERCISE 1:\n==========\n");
 let submitSearch = document.getElementById("submitSearch");
 let searchWord = document.getElementById("searchWord");
-let imageContainer = document.getElementById("imageContainer");
+let imageContainer = document.querySelector("#imageContainer > img");
 let feedbackParagraph = document.getElementById("feedbackParagraph");
 let giphyURL = `https://api.giphy.com/v1/gifs/translate`;
 let APIKEY = `0z0AH2s7I80puVvSwg5jeyzJ8CUJnQav`;
-let S = searchWord.value;
 
 submitSearch.addEventListener("click", (event) => {
-    console.log("hello");
-fetch(`${giphyURL}?api_key=${APIKEY}&s=${S}`);
+fetch(`${giphyURL}?api_key=${APIKEY}&s=${searchWord.value}&weirdness=10`)
+.then((res) => res.json())
+.then((body) => {
+    imageContainer.src = body.data.images.original.url;
+})
+.catch((err) => console.error(err));
 });
 
