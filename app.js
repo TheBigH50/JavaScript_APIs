8a325e87f9a5a8265e122665f6171250780d46f8
TheBigH50 <HFangmeier@outlook.com>
2023-01-20 10:42:59 -0600

    what

--- app.js
+++ app.js
@@ -2,22 +2,5 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
-let submitSearch = document.getElementById("submitSearch");
-let searchWord = document.getElementById("searchWord");
-let imageContainer = document.querySelector("#imageContainer > img");
-let feedbackParagraph = document.getElementById("feedbackParagraph");
-let giphyURL = `https://api.giphy.com/v1/gifs/translate`;
-let APIKEY = `0z0AH2s7I80puVvSwg5jeyzJ8CUJnQav`;
 
-submitSearch.addEventListener("click", (event) => {
-fetch(`${giphyURL}?api_key=${APIKEY}&s=${searchWord.value}&weirdness=10`)
-.then((res) => res.json())
-.then((body) => {
-    imageContainer.src = body.data.images.original.url;
-})
-.catch((err) => {
-    console.error(err);
-    feedbackParagraph.textContent = err.message;
-});
-});
 
