332f7f72cf17aadb5b6d268b049c6c4ea4dc5282
TheBigH50 <HFangmeier@outlook.com>
2023-01-20 12:07:45 -0600

    redo during class

--- app.js
+++ app.js
@@ -3,4 +3,28 @@ console.log("Hello World!\n==========\n");
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
 
+function displayAnImage() {
+    fetch(
+        `https://api.giphy.com/v1/gifs/translate?api_key=0z0AH2s7I80puVvSwg5jeyzJ8CUJnQav&s=${searchInput.value}`
+      )
+        .then((res) => {
+          console.log(res);
+          return res.json();
+        })
+        .then((imageBlob) => {
+          console.log(imageBlob);
+          const imgHolder = document.getElementById("gif");
+          imgHolder.src = imageBlob.data.images.original.url;
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+      
+}
 
+const submitSearchButton = document.getElementById("submitSearch");
+const searchInput = document.getElementById("searchWord");
+submitSearchButton.addEventListener("click", displayAnImage);
+const otherSearchWord = document.getElementById("searchWord");
+
+console.log(otherSearchWord);
